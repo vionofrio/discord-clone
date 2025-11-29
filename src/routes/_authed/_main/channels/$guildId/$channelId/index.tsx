f26a2c36feb817cd4aab7d3ebd39e98cb2dfa8d9
vionofrio <ovictor.valle@outlook.com>
2025-11-29 15:06:10 -0300

    feat: add `_main` route structure

---
+++ src/routes/_authed/_main/channels/$guildId/$channelId/index.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute(
+  "/_authed/_main/channels/$guildId/$channelId/",
+)({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return <div>Hello "/_authed/_main/channels/$guildId/$channelId/"!</div>;
+}
